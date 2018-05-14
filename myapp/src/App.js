bef756aa901ece74f93978085653b57c33b5b025
csong111 <caroline.song@mail.mcgill.ca>
2018-05-14 18:59:02 -0400

    setup

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Account from './Account.js';
+import SignIn from './SignIn.js';
+import SignUp from './SignUp.js';
+import ArtistSignUp from './ArtistSignUp.js';
+import ArtistProfile from './ArtistProfile.js';
+import Cart from './Cart.js';
+import ItemDetail from './ItemDetail.js';
+import CreateListing from './CreateListing.js'; 
+import RoomView from './RoomView.js'; 
+
 
 class App extends Component {
   render() {
