d0a76b7195e124e70b6e2fb9617116a5cc3597c8
csong111 <caroline.song@mail.mcgill.ca>
2018-05-14 19:45:22 -0400

    updates

--- myapp/src/ArtistSignUp.js
+++ myapp/src/ArtistSignUp.js
@@ -3,16 +3,63 @@ import logo from './logo.svg';
 import './App.css';
 
 class ArtistSignUp extends Component {
+    constructor () {
+        super();
+        this.state={
+            nameInput: "",
+            emailInput: "",
+            passInput: "",
+            artistInput: "",
+            cityInput: ""
+        }
+    }
+
+    handleCity = (event) => {
+        this.setState({cityInput: event.target.value});
+    }
+
+    handleArtist = (event) => {
+        this.setState({artistInput: event.target.value});
+    }
+
+    handlePass = (event) => {
+        this.setState({passInput: event.target.value});
+    }
+
+    handleEmail = (event) => {
+        this.setState({emailInput: event.target.value});
+    }
+
+    handleName = (event) => {
+        this.setState({nameInput: event.target.value});
+    }
+
+    uploadFile = (event) => {
+
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+    }
+
     render() {
       return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+        <div className="ArtistSign">
+        <h1>I'M AN ARTIST</h1>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Referral code"/>
+                <div>OR</div>
+                <input type="text" onChange={this.handleName} value={this.state.nameInput} placeholder="Name" required/>
+                <input type="text" onChange={this.handleEmail} value={this.state.emailInput} placeholder="name@email.com" required/>
+                <input type="text" onChange={this.handlePass} value={this.state.passInput} placeholder="Password" required/>
+                <input type="text" onChange={this.handleArtist} value={this.state.artistInput} placeholder="Artist Description" required/>
+                <input type="text" onChange={this.handleCity} value={this.state.cityInput} placeholder="City, Country" required/>
+                <input type="file" onChange={event => this.uploadFile(event.target.files[0])} placeholder="Upload Art" required/>
+                <input type="file" onChange={event => this.uploadFile(event.target.files[1])} placeholder="Upload Art" required/>
+                <input type="file" onChange={event => this.uploadFile(event.target.files[2])} placeholder="Upload Art" required/>
+                <input type="submit"/>
+            </form>
         </div>
       );
     }
--- myapp/src/Stripe.js
+++ myapp/src/Stripe.js
@@ -7,46 +7,14 @@ import StripeCheckout from 'react-stripe-checkout';
 export default class TakeMoney extends React.Component {
   onToken = (token) => { 
     console.log(token)
-  /*onToken = (token) => {
-    fetch('/save-stripe-token', {
-      method: 'POST',
-      body: JSON.stringify(token),
-    }).then(response => {
-      response.json().then(data => {
-        alert(`We are in business, ${data.email}`);
-      });
-    });*/
   }
-  /*
-  onToken = (token) => {
-    alert("ASD")
-    fetch('/save-stripe-token', {
-      method: 'POST',
-      body: JSON.stringify(token),
-    })
-    .then(e => e.text())
-    .then(e => console.log(e))}*/
-//    .then(e => JSON.parse(e))
-
-
-
-      /*
-      console.log("ASD");
-      return response.json()}).then(data => {
-        alert(`We are in business, ${data.email}`);
-      });*/
-    
   
-
-  // ...
-
   render() {
     return (
       // ...
       <StripeCheckout
         token={this.onToken}
         stripeKey="pk_test_worrUeYzGQeY1wmpIgxohWx6"
-        
       />
     )
   }
