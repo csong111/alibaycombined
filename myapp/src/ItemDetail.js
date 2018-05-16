5c6fe02ee681b1e626356cd7a8907554162e2e5d
aishad <aishadiallo@gmail.com>
2018-05-16 19:13:14 -0400

    updates

--- myapp/src/ItemDetail.js
+++ myapp/src/ItemDetail.js
@@ -20,7 +20,6 @@ class ItemDetail extends Component {
       price: ""
     };
   }
-<<<<<<< HEAD
 
   //getItem details
   componentDidMount = () =>{
@@ -28,15 +27,6 @@ class ItemDetail extends Component {
   }
   addToCart = itemID => {};
   seeArtistInfo = artistName => {};
-=======
-  addToCart = itemID => {
-    //fetch addtocart
-
-  };
-  seeArtistInfo = artistName => {
-    //fetch getartistdetails
-  };
->>>>>>> 56be73041b4128f9bd8154c09b30d79a45323abd
 
   render() {
       //fetch itemdetails from backend
