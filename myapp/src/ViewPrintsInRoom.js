f70b0762b2994b751c82936a2baa65ecaff6fe77
Jen <jenodowd@me.com>
2018-05-22 15:09:52 -0400

    update

--- myapp/src/ViewPrintsInRoom.js
+++ myapp/src/ViewPrintsInRoom.js
@@ -11,7 +11,7 @@ import SearchBar from "./page-elements.js/search-bar.js";
 import UserAccount from "./UserAccount.js";
 import UserAccountButton from "./page-elements.js/user-account-button.js";
 import { BrowserRouter, withRouter, Route, Link } from "react-router-dom";
-import Item from "./page-elements.js/Item.js"
+import Item from "./page-elements.js/Item.js";
 
 class ViewPrintsInRoom extends Component {
   constructor() {
@@ -21,24 +21,23 @@ class ViewPrintsInRoom extends Component {
       img2: "",
       img3: "",
       itemsInCat: [],
+      canMove: false,
+      imgX: 150,
+      imgY: 150,
+      innerDivHeight:100,
+      innerDivWidth: 100,
     };
   }
 
   componentDidMount = () => {
-    fetch("/getCatItems?cat="+"Prints", {
-      method: 'GET'
-    }).then(res=>res.text())
-      .then(resB=>{
-        let parsed=JSON.parse(resB)
-        console.log(parsed)
-        this.setState({itemsInCat: parsed})
-      })
-  };
-
-
-  //Fetch create listing.
-  handleSubmit = event => {
-    event.preventDefault();
+    fetch("/getCatItems?cat=" + "Prints", {
+      method: "GET"
+    })
+      .then(res => res.text())
+      .then(resB => {
+        let parsed = JSON.parse(resB);
+        this.setState({ itemsInCat: parsed });
+      });
   };
 
   uploadFile = (x, stateName) => {
@@ -54,24 +53,148 @@ class ViewPrintsInRoom extends Component {
       .then(() => this.state.imageInput);
   };
 
-  showInRoom = (img) => {
-    this.setState({img2: img})
-  }
+  showInRoom = img => {
+    this.setState({ img2: img });
+  };
+
+  moveImage = e => {
+    var dx = e.clientX - this.state.iniX;
+    var dy = e.clientY - this.state.iniY;
+
+    function offset(el) {
+      var rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+    }
+
+    var in_ = document.getElementById("moveImg");
+    var out_ = document.getElementById("outImg");
+
+    var inx = offset(in_).left
+    var iny = offset(in_).top
+    var inX = inx + in_.clientWidth;
+    var inY = iny + in_.clientHeight;
+
+
+    var outx = offset(out_).left
+    var outy = offset(out_).top
+    var outX = outx + out_.clientWidth;
+    var outY = outy + out_.clientHeight;
+
+
+    if(
+      inx+dx < outx ||
+      iny+dy < outy ||
+      inX+dx > outX ||
+      inY+dy > outY
+    ){
+      this.setState({canMove : false})
+    }else{
+      this.setState({
+        iniX: e.clientX,
+        iniY: e.clientY,
+        imgX: this.state.imgX + dx,
+        imgY: this.state.imgY + dy
+      });
+    }
+  };
+
+  // setInnerDiv = () => {
+  //   if(this.state.img2){
+  //     var in_ = document.getElementById("moveImg");
+  //     let y = in_.clientHeight 
+  //     let x = in_.clientWidth 
+  //   } 
+  // }
 
   render() {
-    var itemsRendered = this.state.itemsInCat.map((el,id)=>{
 
+    var fullSize = {
+      width: "200px",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "contain",
+      resize: "both",
+      overflow: "auto",
+      border: "10px solid black 0",
+    };
+
+    var itemsRendered = this.state.itemsInCat.map((el, id) => {
       if (el.cutout) {
-      return (
-        <div className="col-6 col-md-3 col-lg-3 noPad space" key={id}>
-        <div className="center">
-        <Item itemID = {el._id} name = {el.name} price = {el.price} artistName = {el.artistName} img1 = {el.img1} />
-        <button className="centerContent button noPad connect" onClick={() => this.showInRoom(el.cutout)}>SHOW IN ROOM</button>
-        </div>
-        </div>
-      )
-    } else return null;
-    })
+        return (
+          <div className="col-6 col-md-3 col-lg-3 noPad space" key={id}>
+            <div className="center">
+              <Item
+                itemID={el._id}
+                name={el.name}
+                price={el.price}
+                artistName={el.artistName}
+                img1={el.img1}
+              />
+              <button
+                className="centerContent button noPad connect"
+                onClick={() => this.showInRoom(el.cutout)}
+              >
+                SHOW IN ROOM
+              </button>
+            </div>
+          </div>
+        );
+      } else return null;
+    });
+
+    var moveImg = this.state.img2 ?
+     this.state.canMove === false ? (
+      <div
+        className="img2"
+        id="moveImg"
+        onMouseDown={e => {
+          console.log("AAAA")
+          this.setState({
+            canMove: true,
+            iniX: e.clientX,
+            iniY: e.clientY
+          });
+        }}
+        onMouseUp={e => {
+          this.setState({
+            canMove: false,
+            iniX: e.clientX,
+            iniY: e.clientY
+          });
+        }}
+        style={{
+          ...fullSize,
+          backgroundImage: "url(" + this.state.img2 + ")",
+          top: this.state.imgY,
+          left: this.state.imgX
+        }}
+      >
+      <img src={this.state.img2} style={{visibility: "hidden",width: "200px"}} /></div>
+    ) : (
+      <div
+        className="img2"
+        id="moveImg"
+        onMouseMove={this.moveImage}
+        onMouseUp={e => {
+          this.setState({
+            canMove: false,
+            iniX: e.clientX,
+            iniY: e.clientY
+          });
+        }}
+        style={{
+          backgroundImage: "url(" + this.state.img2 + ")",
+          ...fullSize,
+          top: this.state.imgY,
+          left: this.state.imgX
+        }}
+      >
+      <img src={this.state.img2} style={{visibility: "hidden",width: "200px"}} />
+      </div>
+    ) : null
+
+
 
     return (
       <div>
@@ -111,46 +234,63 @@ class ViewPrintsInRoom extends Component {
 
         <div>
           <h2 className="catName">VIEW IN ROOM</h2>
-          <form>
-            <img className="img2" height="200px" src={this.state.img2} />
-            {this.state.img1 ? <img width="700px" src={this.state.img1}/> : <img width="700px" src="room/test.jpg" /> }
+
+          {this.state.img1 ? (
+          <div style={{
+            width: "700px",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "100%",
+            backgroundSize: "700px auto",
+            backgroundImage: "url(" + this.state.img1  + ")",
+          }}
+            // onMouseMove={this.setInnerDiv}
+          >
+            <img id="outImg" width="700px" src={this.state.img1} style={{visibility:"hidden"}} />
+            {moveImg}
+          </div>
+          ) : (
+          <div style={{
+            width: "700px",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "100%",
+            backgroundSize: "700px auto",
+            backgroundImage: "url(" + "room/test.jpg"  + ")",
+          }}
+            // onMouseMove={this.setInnerDiv}
+          >
+            <img id="outImg" width="700px" src="room/test.jpg" style={{visibility:"hidden"}} />
+            {moveImg}
+          </div>
+          )}
+
+          <input
+            id="createListingImg1"
+            style={{ display: "none" }}
+            type="file"
+            onChange={event => this.uploadFile(event.target.files[0], "img1")}
+            placeholder="Upload Item Image"
+          />
+
+          <input
+            id="createListingImg2"
+            style={{ display: "none" }}
+            type="file"
+            onChange={event => this.uploadFile(event.target.files[0], "img2")}
+            placeholder="Upload Item Image"
+          />
 
 
-            {/* HIDDEN!!!!!!!! */}
+          <button
+            type="button"
+            className="button noPad connect"
+            onClick={() => {
+              document.getElementById("createListingImg1").click();
+            }}
+          >
+            CHANGE BACKGROUND IMAGE
+          </button>
 
-            <input
-              id="createListingImg1"
-              style={{ display: "none" }}
-              type="file"
-              onChange={event => this.uploadFile(event.target.files[0], "img1")}
-              placeholder="Upload Item Image"
-            />
-
-            <input
-              id="createListingImg2"
-              style={{ display: "none" }}
-              type="file"
-              onChange={event => this.uploadFile(event.target.files[0], "img2")}
-              placeholder="Upload Item Image"
-            />
-
-            {/* ///////////// */}
-
-            <br />
-            <button
-              type="button"
-              className="button noPad connect"
-              onClick={() => {
-                document.getElementById("createListingImg1").click();
-              }}
-            >
-              CHANGE BACKGROUND IMAGE
-            </button>
-
-          </form>
-          <div className="row">
-          {itemsRendered}
-        </div>
+          <div className="row">{itemsRendered}</div>
         </div>
       </div>
     );
